17102a1218b82b17b258185d326c6648afd0a8c2
Andrew Demchenko <demch.andrew@gmail.com>
2023-04-17 20:00:27 +0200

    Created separate files for different functions #1

--- src/WIOTerminal/WIOTerminal.ino
+++ src/WIOTerminal/WIOTerminal.ino
@@ -1,73 +1,23 @@
-#include"TFT_eSPI.h"
 #include <Ultrasonic.h>
 #include "Arduino.h"
+#include "Ultrasonic.hpp"
+#include "Screen.hpp"
 
-// Initializing the ultrasonic sensors on pins D0 and D2
-Ultrasonic ultrasonic1(0);
-Ultrasonic ultrasonic2(2);
+int countMain = 0;
 
-// Initializing the TFT screen
-TFT_eSPI tft;
-
-int count = 0;
-
-int time1 = 0;
-int time2 = 0;
-
-int threshHold = 200;
 int delayTime = 500;
 
 void setup()
 {
-    // Screen setup
-    tft.begin();
-    tft.setRotation(3);
-
-    tft.fillScreen(TFT_WHITE); //background
-
-    tft.setTextColor(TFT_BLUE);
-    tft.setTextSize(5);
-    tft.drawString("INIT", 50, 50);
+    initialize();
 }
 void loop()
 {
-    long rangeInCentimeters1;
-    long rangeInCentimeters2;
+    UltrasonicData data = detectMovement(countMain);
 
-    rangeInCentimeters1 = ultrasonic1.MeasureInCentimeters(); // two measurements should keep an interval
-    delay(50);
-    rangeInCentimeters2 = ultrasonic2.MeasureInCentimeters();
+    countMain = data.count;
 
-    // checking if the person is passing
-    if(rangeInCentimeters1 < threshHold) {
-        time1 = millis();
-    }
-
-    if(rangeInCentimeters2 < threshHold) {
-        time2 = millis();
-    }
-
-    // ! code inspired by https://www.sensingthecity.com/detecting-movement-direction-with-two-ultrasonic-distance-sensors/
-    if (time1 > 0 && time2 > 0) {       // if both sensors have nonzero timestamps
-        if (time1 < time2) {                      // if left sensor triggered first
-            count++;    // direction is left to right
-        } 
-        else if (time2 < time1) {                 // if right sensor triggered first
-            count--;    // direction is right to left
-        }
-
-        // after printing direction, reset both timestamps to 0
-        time1 = 0;
-        time2 = 0;
-    }
-
-    if(count < 0){  //prevent the count to go below 0
-        count = 0;
-    }
-
-    tft.fillScreen(TFT_WHITE); //background
-    tft.drawString(String(count), 50, 70);
-    tft.drawString(String(rangeInCentimeters1), 50, 120);
-    tft.drawString(String(rangeInCentimeters2), 50, 170);
+    displayPeopleCountDebug(data.count, data.distance1, data.distance2);
+    
     delay(delayTime);
 }
