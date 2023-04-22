ee003f5828371c1bd8735931234c1f77f6b42466
cch0000 <guscampca@student.gu.se>
2023-04-22 14:30:12 +0200

    Refactored the comments to match the style of the other files

--- src/WIOTerminal/TempHumidity.cpp
+++ src/WIOTerminal/TempHumidity.cpp
@@ -7,15 +7,20 @@
 
 DHT tempHumiditySensor(DHTPIN, DHTTYPE);
 
+/// @brief Initialise the temperature and humidity sensor
 void tempHumidInit() {
     Serial.begin(9600);
-    tempHumiditySensor.begin();     //Initialize the sensor
+    tempHumiditySensor.begin();
 }
 
+/// @brief Measure temperature in celsius
+/// @return Temperature as an integer
 int measureTemperature() { 
-    return tempHumiditySensor.readTemperature(false);    //Measure temperature in celsius
+    return tempHumiditySensor.readTemperature(false);
 }
 
+/// @brief Measures humidity
+/// @return Humidity as an integer in a scale from 0 to 100
 int measureHumidity() {
-    return tempHumiditySensor.readHumidity();   //Measure humidity
+    return tempHumiditySensor.readHumidity(); 
 }
