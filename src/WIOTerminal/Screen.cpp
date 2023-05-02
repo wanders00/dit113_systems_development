6ef551e6fad2702c1afd330763b77642bbb7ecec
William Andersson <willand@student.chalmers.se>
2023-05-02 19:02:37 +0200

    Implements #2
    Revisited almost the entirety of the WIO terminals MqttClient to make it
    work more nicely with other parts of the system. This translates to:
    less system wide delays.
    
    Known issues:
    Whenever the WIO attempts to reconnect to the mqtt broker it will be a
    system wide pause until it reconnects. This can unfortunately not be
    fixed, as it is how the mqtt library we use implementation.
    
    Other changes:
    1. Revamped buzzer functionality to better work as an alert.
    2. Added Util class to manage time management. This to not have the
    current time of the system differentiate due to computation time.
    3. Renamed some variables & improved some comments.

--- src/WIOTerminal/Screen.cpp
+++ src/WIOTerminal/Screen.cpp
@@ -1,8 +1,12 @@
 // Fonts taken from https://github.com/lakshanthad/Wio_Terminal_Classroom_Arduino/blob/main/Classroom%203/Free_Fonts_Example/Free_Fonts.h
-
-#include "Screen.hpp"
-#include "TFT_eSPI.h"
 #include "fonts/Free_Fonts.h"
+
+// Arduino libraries
+#include "TFT_eSPI.h"
+
+// Local header files
+#include "MqttClient.hpp"
+#include "Screen.hpp"
 
 const uint32_t BACKGROUND_COLOR = TFT_WHITE;
 const uint32_t TEXT_COLOR = TFT_BLUE;
@@ -24,6 +28,18 @@ void screenInit() {
     tft.setTextColor(TEXT_COLOR);
     tft.setFreeFont(TEXT_FONT_STYLE);
     tft.drawString("SCREEN INIT", 50, 50);
+}
+
+/**
+ * Screen loop.
+ *
+ * @note So far only shows wifi/mqtt connection
+ * @return void
+ */
+void updateScreen() {
+    flashScreen();
+    tft.drawString("Wifi status: " + String(getWifiConnection()), 0, 0);
+    tft.drawString("mqtt status: " + String(getMqttConnection()), 0, 30);
 }
 
 /**
@@ -62,6 +78,18 @@ void displayPeopleCountDebug(int count, int dis1, int dis2) {
 void displayMessage(String message) {
     flashScreen();
     tft.drawString(message, 0, 0);
+}
+
+/**
+ * Displays the message argument on the screen and sets the background to red.
+ *
+ * @param alertMessage The message to be displayed.
+ * @return void
+ */
+void displayAlert(String alertMessage) {
+    flashScreen();
+    tft.fillScreen(TFT_RED);
+    tft.drawString(alertMessage, 0, 0);
 }
 
 /**
