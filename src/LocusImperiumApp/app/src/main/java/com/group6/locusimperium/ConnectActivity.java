7c2b03dbb38b4c175457cc6afc0e919b13b22f8a
Joshua Falck <cjoshua@chalmers.se>
2023-05-02 19:43:22 +0200

    added save button with saving functionallity #47
    still lacks the functionallity to use the saved IP address as a wifi connection

--- src/LocusImperiumApp/app/src/main/java/com/group6/locusimperium/ConnectActivity.java
+++ src/LocusImperiumApp/app/src/main/java/com/group6/locusimperium/ConnectActivity.java
@@ -1,13 +1,25 @@
 package com.group6.locusimperium;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+import static com.group6.locusimperium.SettingsActivity.SHARED_PREFS;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ConnectActivity extends AppCompatActivity {
+    Context context;
+    private Button saveIPButton;
+    private EditText inputIP;
+    private String ipaddress;
+    public static final String IPADDRESS = "ipaddress";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +45,41 @@ public class ConnectActivity extends AppCompatActivity {
 
             }
         });
+        inputIP = (EditText) findViewById(R.id.inputIPAddress);
+        saveIPButton = (Button) findViewById(R.id.saveIP);
+        saveIPButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * saves data when save button is clicked
+             * @return void
+             */
+            @Override
+            public void onClick(View view) {
+                saveIP();
+            }
+        });
+
+        loadIP();
+        updateIP();
     }
+
+
+    public void saveIP() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(IPADDRESS, inputIP.getText().toString());
+        editor.apply();
+
+        Toast.makeText(this, "Saved IP", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loadIP() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        ipaddress = sharedPreferences.getString(IPADDRESS, "");
+    }
+
+    public void updateIP() {
+        inputIP.setText(ipaddress);
+    }
+
 }
